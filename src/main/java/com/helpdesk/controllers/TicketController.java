2dfce205d97fd15112a2e7e403116f2c50a09687
arisnel <arisnel21@gmail.com>
2026-09-24 21:13:38 -0300

    se finaliza construccion de los 5 endpoints CRUD

--- src/main/java/com/helpdesk/controllers/TicketController.java
+++ src/main/java/com/helpdesk/controllers/TicketController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.helpdesk.models.Ticket;
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController //Spring esta clase va a recibir peticiones HTTP de los clientes y respundera mediante una API REST
 public class TicketController {
@@ -20,6 +24,21 @@ public class TicketController {
     public List<Ticket> listarTickets() {// endpoint para leer
         return ticketService.listarTickets();
     }
+
+    @GetMapping("/tickets/{id}") // busca por un ticket en especifico
+    public Optional<Ticket> buscarPorId(@PathVariable Long id){  //optional previene el NullPointerException (indica que el resultado existe o no)
+        return ticketService.buscarPorId(id);
+    }
+
+    @PutMapping("/tickets/{id}")
+    public Ticket actualizarTicket(@PathVariable Long id,@RequestBody Ticket ticketActualizado){
+        return ticketService.actualizarTicket(id,ticketActualizado);
+    }
+    @DeleteMapping("/tickets/{id}")
+    public void  eliminarTicket(@PathVariable Long id){
+        ticketService.eliminarTicket(id);
+    }
+
 
     @PostMapping("/tickets")// endpoint para crear
     public Ticket crearTicket(@RequestBody Ticket ticket){// este es el endpoint para crear un nuevo ticket
